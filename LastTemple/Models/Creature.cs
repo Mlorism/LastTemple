a3fc1bd5358acdce91535b016db50e971a6aef01
Mlorism <Mlorism@gmail.com>
2021-01-17 00:46:12 +0100

    initial commit

---
+++ LastTemple/Models/Creature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LastTemple
+{
+	public class Creature
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		#region Attributes
+		public int Strength { get; set; }
+		public int Endurance { get; set; }
+		public int Willpower { get; set; }
+		public int Speed { get; set; }
+		public int Agility { get; set; }
+		#endregion
+
+		#region Derived statistics
+		public int HitPoints { get; set; } // Derived from Endurance and Strength
+		public int Mana { get; set; } // Derived from Willpower
+		public int ActionPoints { get; set; } // Derived from Speed
+		public int DogeChance { get; set; } // Derived from Agility
+		public int DamageResistance { get; set; } // Derived from Endurance
+		public int MagicResistance { get; set; } // Derived from Willpower
+		public int ArmorClass { get; set; } // Derived from equipped armor or natural hide/shell
+		public int Initiative { get; set; } // Derived from Speed and Agility
+		#endregion
+	}
+}
