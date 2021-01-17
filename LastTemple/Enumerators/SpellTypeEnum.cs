813addd5b4de57a9a574238a2e7d53b88de56741
Mlorism <Mlorism@gmail.com>
2021-01-17 11:00:00 +0100

    Enums created, Weapon class implemented

---
+++ LastTemple/Enumerators/SpellTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LastTemple.Enumerators
+{
+	public enum SpellTypeEnum
+	{
+		Offensive, // Damage enemy
+		Defensive, // Increase armor calass
+		Boost, // Increase temporary attributes or derived statistics
+		Healing // Restore hit points
+	}
+}
--- LastTemple/Models/Weapon.cs
+++ LastTemple/Models/Weapon.cs
@@ -7,5 +7,10 @@ namespace LastTemple.Models
 {
 	public class Weapon
 	{
+		public int Id { get; set; }
+		public int Name { get; set; }
+		public int BaseDamage { get; set; }
+		public int BaseActionCost { get; set; }
+		public int BaseHitChance { get; set; }
 	}
 }
