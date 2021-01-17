4beca8ce5a300cc83eefbd4669a300f8f7dce306
Mlorism <Mlorism@gmail.com>
2021-01-17 10:36:26 +0100

    new models and changes in creature model

--- LastTemple/Models/Creature.cs
+++ LastTemple/Models/Creature.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LastTemple.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,22 +12,28 @@ namespace LastTemple
 		public string Name { get; set; }
 
 		#region Attributes
-		public int Strength { get; set; }
-		public int Endurance { get; set; }
-		public int Willpower { get; set; }
-		public int Speed { get; set; }
-		public int Agility { get; set; }
+		public int Strength { get; set; } // can be increased as the character level up
+		public int Endurance { get; set; } // can be increased as the character level up
+		public int Willpower { get; set; } // can be increased as the character level up
+		public int Speed { get; set; } // the upper limit is 10 and cannot be changed even after level up, only by modifiers / events
+		public int Agility { get; set; } // the upper limit is 10 and cannot be changed even after level up, only by modifiers / events
 		#endregion
 
 		#region Derived statistics
 		public int HitPoints { get; set; } // Derived from Endurance and Strength
 		public int Mana { get; set; } // Derived from Willpower
 		public int ActionPoints { get; set; } // Derived from Speed
+		public int BaseDamage { get; set; } // Derived from Weapon
 		public int DogeChance { get; set; } // Derived from Agility
 		public int DamageResistance { get; set; } // Derived from Endurance
 		public int MagicResistance { get; set; } // Derived from Willpower
-		public int ArmorClass { get; set; } // Derived from equipped armor or natural hide/shell
+		public int ArmorClass { get; set; } // Derived from equipped armor or natural hide/shell => property Armor
 		public int Initiative { get; set; } // Derived from Speed and Agility
+		#endregion
+
+		#region Inventory
+		public Weapon Weapon { get; set; }
+		public Armor Armor { get; set; }
 		#endregion
 	}
 }
