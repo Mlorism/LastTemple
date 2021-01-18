ee94799bd6cc01a41675a51e5ff4e6d835ca3040
Mlorism <Mlorism@gmail.com>
2021-01-18 23:45:49 +0100

    EditArmor implemented

--- LastTemple/CRUD/Armors/EditArmor.cs
+++ LastTemple/CRUD/Armors/EditArmor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LastTemple.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,27 @@ namespace LastTemple.CRUD
 {
 	public class EditArmor
 	{
+		private readonly ApplicationDbContext _ctx;
+
+		public EditArmor(ApplicationDbContext ctx)
+		{
+			_ctx = ctx;
+		}
+
+		public async Task<bool> Update(Armor armor)
+		{
+			Armor target = _ctx.Armors.Find(armor.Id);
+
+			if (target == null) return false;
+
+			target.Name = armor.Name;
+			target.DamageResistance = armor.DamageResistance;
+			target.MagicResistance = armor.MagicResistance;
+
+			await _ctx.SaveChangesAsync();
+
+			return true;
+		} // Update()
 
 	}
 }
