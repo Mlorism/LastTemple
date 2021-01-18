01979d5058a756eb743d617766298fc6ac0faccf
Mlorism <Mlorism@gmail.com>
2021-01-18 23:35:28 +0100

    GetArmors implemented

--- LastTemple/CRUD/Armors/GetArmors.cs
+++ LastTemple/CRUD/Armors/GetArmors.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LastTemple.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,16 @@ namespace LastTemple.CRUD
 {
 	public class GetArmors
 	{
+		private readonly ApplicationDbContext _ctx;
 
+		public GetArmors(ApplicationDbContext ctx)
+		{
+			_ctx = ctx;
+		}
+
+		public IEnumerable<Armor> Get()
+		{
+			return _ctx.Armors.ToList();
+		} // Get()
 	}
 }
