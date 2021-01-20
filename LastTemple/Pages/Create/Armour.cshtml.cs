f112d8746f40982654a19374d2c977d114e6030e
Mlorism <Mlorism@gmail.com>
2021-01-20 21:48:07 +0100

    Armour managment adding form and styling

--- LastTemple/Pages/Create/Armour.cshtml.cs
+++ LastTemple/Pages/Create/Armour.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LastTemple.CRUD;
 using LastTemple.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,10 +11,29 @@ namespace LastTemple.Pages.Create
 {
 	public class ArmourModel : PageModel
 	{
+		private readonly ApplicationDbContext _ctx;
+
+		public ArmourModel(ApplicationDbContext ctx)
+		{
+			_ctx = ctx;
+		}
+
+		[BindProperty]
 		public Armor Armor { get; set; }
 		public void OnGet()
 		{
 
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();				
+			}
+
+			await new CreateArmor(_ctx).Create(Armor);
+			return Page();
 		}
 	}
 }
