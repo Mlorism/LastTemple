5c3ecd3727e03b1167ec80b1d22682b73d26e7f5
Mlorism <Mlorism@gmail.com>
2021-01-29 19:54:04 +0100

    Weapon page sketch, Post/Delete/Update methods implemented

--- LastTemple/Pages/Manage/Weapon.cshtml.cs
+++ LastTemple/Pages/Manage/Weapon.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LastTemple.CRUD;
 using LastTemple.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +13,7 @@ namespace LastTemple.Pages.Create
     {
 		private ApplicationDbContext _ctx;
 
+        [BindProperty]
 		public Weapon Weapon { get; set; }
 		public IEnumerable<Weapon> Weapons { get; set; }
 
@@ -21,7 +23,43 @@ namespace LastTemple.Pages.Create
 		}
         public void OnGet()
         {
-
+            Weapons = new GetWeapons(_ctx).Get();
         }
+
+        public async Task<IActionResult> OnPostCreateAsync()
+		{
+            await new CreateWeapon(_ctx).Create(Weapon);
+
+            return RedirectToPage("Weapon");
+		}
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+		{
+            var item = _ctx.Weapons.Find(id);
+
+            if (item == null)
+			{
+                return RedirectToPage("Weapon");
+            }
+
+            await new DeleteWeapon(_ctx).Delete(id);
+
+            return RedirectToPage("Weapon");
+        }
+
+        public async Task<IActionResult> OnPostUpdateAsync()
+		{
+            var item = _ctx.Weapons.Find(Weapon.Id);
+
+            if (item == null)
+            {
+                return RedirectToPage("Weapon");
+            }
+
+            await new EditWeapon(_ctx).Update(Weapon);
+
+            return RedirectToPage("Weapon");
+        }
+
     }
 }
