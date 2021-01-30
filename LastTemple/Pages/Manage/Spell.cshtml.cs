247c6f1a06cebbbadff4373fa616ba90edd69511
Mlorism <Mlorism@gmail.com>
2021-01-30 13:29:59 +0100

    CRUD for spells

--- LastTemple/Pages/Manage/Spell.cshtml.cs
+++ LastTemple/Pages/Manage/Spell.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LastTemple.CRUD;
+using LastTemple.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,57 @@ namespace LastTemple.Pages.Create
 {
     public class SpellModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _ctx;
+
+        public SpellModel(ApplicationDbContext ctx)
         {
+            _ctx = ctx;
         }
+
+        [BindProperty]
+		public Spell Spell { get; set; }
+		public IEnumerable<Spell> Spells { get; set; }
+
+		public void OnGet()
+        {
+            Spells = new GetSpells(_ctx).Get();
+        }
+
+        public async Task<IActionResult> OnPostCreateAsync()
+		{
+            await new CreateSpell(_ctx).Create(Spell);
+
+            return RedirectToPage("Spell");
+		}
+
+        public async Task<IActionResult> OnPostDeleteAsync()
+		{
+            var item = _ctx.Spells.Find(Spell.Id);
+
+            if (item == null)
+			{
+                return RedirectToPage("Spell");
+            }
+
+            await new DeleteSpell(_ctx).Delete(Spell.Id);
+
+            return RedirectToPage("Spell");
+
+        }
+
+        public async Task<IActionResult> OnPostUpdateAsync()
+		{
+            var item = _ctx.Spells.Find(Spell.Id);
+
+            if (item == null)
+            {
+                return RedirectToPage("Spell");
+            }
+
+            await new EditSpell(_ctx).Update(Spell);
+
+            return RedirectToPage("Spell");
+        }
+           
     }
 }
