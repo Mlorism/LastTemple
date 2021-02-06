84f3e9a2e4ee5be170cade3dada5d20154109787
Mlorism <Mlorism@gmail.com>
2021-02-06 13:17:48 +0100

    post/get creature managment page

--- LastTemple/Pages/Manage/Creature.cshtml.cs
+++ LastTemple/Pages/Manage/Creature.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LastTemple.CRUD;
+using LastTemple.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,65 @@ namespace LastTemple.Pages.Create
 {
     public class CreatureModel : PageModel
     {
-        public void OnGet()
+		private ApplicationDbContext _ctx;
+
+		public CreatureModel(ApplicationDbContext ctx)
+		{
+            _ctx = ctx;
+		}
+
+        [BindProperty]
+        public Creature Creature { get; set; }
+		public IEnumerable<Creature> Creatures { get; set; }
+
+		public void OnGet()
         {
+            Creatures = new GetCreatures(_ctx).Get();
         }
+
+        public async Task<IActionResult> OnPostCreateAsync()
+		{
+            await new CreateCreature(_ctx).Create(Creature);
+
+            return RedirectToPage("Creature");
+		}
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+		{
+            var item = _ctx.Creatures.Find(id);
+
+            if (item == null)
+			{
+                return RedirectToPage("Creature");
+            }
+
+            await new DeleteCreature(_ctx).Delete(id);
+
+            return RedirectToPage("Creature");
+        }
+
+        public async Task<IActionResult> OnPostUpdateAsync()
+		{
+            var item = _ctx.Creatures.Find(Creature.Id);
+
+            if (item == null)
+            {
+                return RedirectToPage("Creature");
+            }
+
+            await new EditCreature(_ctx).Update(Creature);
+
+            return RedirectToPage("Creature");
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
