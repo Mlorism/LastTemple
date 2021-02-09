7514735429709206d245606f783bb604f7d15754
Mlorism <Mlorism@gmail.com>
2021-02-09 22:16:29 +0100

    additional methods to update creature

--- LastTemple/CRUD/Creature/EditCreature.cs
+++ LastTemple/CRUD/Creature/EditCreature.cs
@@ -76,7 +76,7 @@ namespace LastTemple.CRUD
 			await _ctx.SaveChangesAsync();
 
 			return true;
-		} // UpdateFull()
+		} // UpdateFull() updates creature's parameters and inventory
 
 		public async Task<bool> UpdateCreature(Creature creature)
 		{
@@ -96,6 +96,20 @@ namespace LastTemple.CRUD
 			target.Speed = creature.Speed;
 			target.Agility = creature.Agility;
 
+			
+			CalculateCreature.DerivedStatistics(creature);
+
+			await _ctx.SaveChangesAsync();
+
+			return true;
+		} // UpdateCreature() updates only creature's parameters
+
+		public async Task<bool> UpdateInventory(Creature creature)
+		{
+			Creature target = _ctx.Creatures.Find(creature.Id);
+
+			if (target == null) return false;
+
 			if (target.Weapon != null)
 			{
 				target.Weapon.Name = creature.Weapon.Name;
@@ -104,15 +118,13 @@ namespace LastTemple.CRUD
 				target.Weapon.ActionCost = creature.Weapon.ActionCost;
 				target.Weapon.HitChance = creature.Weapon.HitChance;
 			}
-			
+
 			if (target.Armor != null)
 			{
 				target.Armor.Name = creature.Armor.Name;
 				target.Armor.DamageResistance = creature.Armor.DamageResistance;
 				target.Armor.MagicResistance = creature.Armor.MagicResistance;
 			}
-			
-
 			target.MagicBook = new List<Spell>();
 			target.Items = new List<Item>();
 
@@ -145,18 +157,96 @@ namespace LastTemple.CRUD
 			await _ctx.SaveChangesAsync();
 
 			return true;
-		} // UpdateCreature()
+		} // UpdateInventory() updates only creature's inventory
 
-		public async Task<bool> UpdateInventory(Creature creature)
+		public async Task<bool> UpdateWeapon(Creature creature)
+		{
+			Creature target = _ctx.Creatures.Find(creature.Id);
+
+			if (target == null) return false;
+			
+			target.Weapon.Name = creature.Weapon.Name;
+			target.Weapon.BaseDamage = creature.Weapon.BaseDamage;
+			target.Weapon.MagicDamage = creature.Weapon.MagicDamage;
+			target.Weapon.ActionCost = creature.Weapon.ActionCost;
+			target.Weapon.HitChance = creature.Weapon.HitChance;			
+			
+			CalculateCreature.DerivedStatistics(creature);
+
+			await _ctx.SaveChangesAsync();
+
+			return true;
+		} // UpdateInventory() updates only creature's inventory
+
+		public async Task<bool> UpdateArmor(Creature creature)
+		{
+			Creature target = _ctx.Creatures.Find(creature.Id);
+
+			if (target == null) return false;			
+			
+			target.Armor.Name = creature.Armor.Name;
+			target.Armor.DamageResistance = creature.Armor.DamageResistance;
+			target.Armor.MagicResistance = creature.Armor.MagicResistance;			
+			
+			CalculateCreature.DerivedStatistics(creature);
+
+			await _ctx.SaveChangesAsync();
+
+			return true;
+		} // UpdateInventory() updates only creature's inventory
+
+		public async Task<bool> UpdateItems(Creature creature)
+		{
+			Creature target = _ctx.Creatures.Find(creature.Id);
+
+			if (target == null) return false;		
+			
+			target.Items = new List<Item>();			
+
+			foreach (var item in creature.Items)
+			{
+				target.Items.Add(new Item
+				{
+					Name = item.Name,
+					ItemType = item.ItemType,
+					Strength = item.Strength,
+					ActionCost = item.ActionCost
+				});
+			}
+
+			CalculateCreature.DerivedStatistics(creature);
+
+			await _ctx.SaveChangesAsync();
+
+			return true;
+		} // UpdateInventory() updates only creature's inventory
+
+		public async Task<bool> UpdateSpells(Creature creature)
 		{
 			Creature target = _ctx.Creatures.Find(creature.Id);
 
 			if (target == null) return false;
 
+			target.MagicBook = new List<Spell>();			
+
+			foreach (var spell in creature.MagicBook)
+			{
+				target.MagicBook.Add(new Spell
+				{
+					Name = spell.Name,
+					Type = spell.Type,
+					Level = spell.Level,
+					ManaCost = spell.ManaCost,
+					ActionCost = spell.ActionCost,
+					Strength = spell.Strength
+				});
+			}
+
+			CalculateCreature.DerivedStatistics(creature);
+
+			await _ctx.SaveChangesAsync();
 
 			return true;
-		} // UpdateInventory()
-
-
+		} // UpdateInventory() updates only creature's inventory
 	}
 }
