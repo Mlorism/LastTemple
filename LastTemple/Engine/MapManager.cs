1ffd8b2fa9b62115ecae03fff437089923aab063
Mlorism <Mlorism@gmail.com>
2021-03-21 13:08:25 +0100

    location marks loaded from map manager

--- LastTemple/Engine/MapManager.cs
+++ LastTemple/Engine/MapManager.cs
@@ -9,5 +9,117 @@ namespace LastTemple.Engine
 	public static class MapManager
 	{
 		public static List<LocationMark> Locations { get; set; }
+
+		public static List<LocationMark> GetLocations()
+		{
+			if (Locations == null)
+			{
+				Locations = new List<LocationMark>();
+				LoadLocations();
+			}
+
+			return Locations;
+		} // GetLocations()
+
+		public static void LoadLocations()
+		{
+			LocationMark location = new LocationMark { 
+			Name = "Świątynia",
+			Width = 0.15,
+			Height = 0.77,
+			Discovered = true,
+			Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Troja",
+				Width = 0.22,
+				Height = 0.15,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Teby",
+				Width = 0.48,
+				Height = 0.83,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Posterunek",
+				Width = 0.63,
+				Height = 0.625,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Bandyci",
+				Width = 0.60,
+				Height = 0.97,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Wyrocznia",
+				Width = 0.63,
+				Height = 0.55,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Biblioteka",
+				Width = 0.42,
+				Height = 0.42,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Krypta",
+				Width = 0.85,
+				Height = 0.5,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+
+			location = new LocationMark
+			{
+				Name = "Katedra",
+				Width = 0.85,
+				Height = 0.12,
+				Discovered = true,
+				Destination = 0
+			};
+
+			Locations.Add(location);
+		} // LoadLocations()
 	}
 }
--- LastTemple/Pages/Gameplay/Map.cshtml.cs
+++ LastTemple/Pages/Gameplay/Map.cshtml.cs
@@ -2,15 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LastTemple.Engine;
+using LastTemple.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LastTemple.Pages.Gameplay
 {
-    public class MapModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class MapModel : PageModel
+	{
+		[BindProperty]
+		public List<LocationMark> Locations { get; set; }
+		public void OnGet()
+		{
+			Locations = MapManager.GetLocations();
+		}
+
+
+
+
+
+	}
 }
